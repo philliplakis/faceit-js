9be1e0dc418b9d55684ffe88829b93e91c7e8f65
Phillip Lakis <p.lakis@icloud.com>
2019-08-29 16:12:09 +1000

    Working update

--- README.md
+++ README.md
@@ -22,18 +22,43 @@ const api = new Faceit(`YOUR_API_KEY`);
 You can now test that your API key is correct and that everything is working by checking for your account information:
 
 ```js
-api.account().then((data) => {
-  console.log(data);
+api.account().then(data => console.log(data));
 ```
 
 If you get back a response similar to the below, success!
 
 ```json
 {
-  "APIKey": "valid"
+  "APIKey": true
 }
 ```
 
 #### Data API
 
-#### Placeholder
+### Issues
+
+Please raise an issue on GitHub with as much information as possible and the steps to replicate (if possible).
+
+### License
+
+The MIT License (MIT)
+
+Copyright (c) 2019 Phillip Lakis
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- index.js
+++ index.js
@@ -1,18 +1,118 @@
 "use strict";
 
 const axios = require("axios");
-let myAPI_key = require("./config.json").apiKey;
-console.log(myAPI_key);
-axios.defaults.baseURL = "https://api.example.com";
-axios.defaults.headers.common["Authorization"] = Bearer + myAPI_key;
-axios.defaults.headers.post["Content-Type"] =
-  "application/x-www-form-urlencoded";
 
-async function account() {
-  try {
-    const response = await axios.get("/games");
-    console.log(response);
-  } catch (error) {
-    console.error(error);
+module.exports = class Faceit {
+  constructor(token) {
+    this.authorization = `Bearer ${token}`;
+    this.apiUrl = "https://open.faceit.com/data/v4";
   }
-}
+
+  _testKey(options) {
+    let promise = new Promise((resolve, reject) => {
+      options.headers = {};
+      options.headers.authorization = `${this.authorization}`;
+      options.headers.content_type = "application/x-www-form-urlencoded";
+      axios(options)
+        .then(data => {
+          resolve({ valid: true });
+        })
+        .catch(err => {
+          reject({ valid: false });
+        });
+    });
+    return promise;
+  }
+
+  _axios(options) {
+    let promise = new Promise((resolve, reject) => {
+      options.headers = {};
+      options.headers.authorization = `${this.authorization}`;
+      options.headers.content_type = "application/x-www-form-urlencoded";
+      axios(options)
+        .then(data => {
+          resolve(data.data);
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
+    return promise;
+  }
+
+  _missing(Param) {
+    let promise = new Promise((resolve, reject) => {
+      reject(`Missing params - ${Param}`);
+    });
+    return promise;
+  }
+
+  account() {
+    let options = {
+      url: `${this.apiUrl}/games?limit=1`
+    };
+    return this._testKey(options);
+  }
+
+  championships(championship_id, option) {
+    let options = {
+      url: `${this.apiUrl}/championships/${championship_id}`
+    };
+    if (option == "matches") options.url = `${options.url}/matches`;
+    if (option == "subs") options.url = `${options.url}/subscriptions`;
+    return this._axios(options);
+  }
+
+  games(offset, limit) {
+    let options = {
+      url: `${this.apiUrl}/games`
+    };
+    if (offset) options.url = `${options.url}?offset=${offset}`;
+    if (limit) options.url = `${options.url}&limit=${limit}`;
+    return this._axios(options);
+  }
+
+  gameId(_id) {
+    if (!_id) return this._missing("game_id");
+    let options = {
+      url: `${this.apiUrl}/games`
+    };
+    return this._axios(options);
+  }
+
+  gameParent(_id) {
+    let options = {
+      url: `${this.apiUrl}/games`
+    };
+    if (_id) options.url = `${options.url}/${_id}`;
+    return this._axios(options);
+  }
+
+  hubs(hub_id, option) {
+    if (!hub_id) return this._missing("hub_id");
+    let options = {
+      url: `${this.apiUrl}/hubs/${hub_id}`
+    };
+    if (option == "matches") options.url = `${options.url}/matches`;
+    if (option == "members") options.url = `${options.url}/members`;
+    return this._axios(options);
+  }
+
+  leaderboards() {
+    let options = {
+      url: `${this.apiUrl}/hubs/${hub_id}`
+    };
+    if (option == "matches") options.url = `${options.url}/matches`;
+    if (option == "members") options.url = `${options.url}/members`;
+    return this._axios(options);
+  }
+
+  matches(match_id, stats) {
+    if (!match_id) return this._missing("match_id");
+    let options = {
+      url: `${this.apiUrl}/matches/${match_id}`
+    };
+    if (stats) options.url = `${options.url}/stats`;
+    return this._axios(options);
+  }
+};
